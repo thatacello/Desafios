543e15fe6b12bd130d3cc16b669aaa48dbf40cad
THAIS CARDOSO DE FARIAS <thais.farias@al.infnet.edu.br>
2021-02-18 14:16:37 -0300

    teste_01 maior número

--- Teste_01/Program.cs
+++ Teste_01/Program.cs
@@ -6,7 +6,62 @@ namespace Teste_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MaiorNumero();
+
+            static void MaiorNumero()
+            {
+                Console.Clear(); // limpa o console
+                Console.ForegroundColor = ConsoleColor.Blue; // troca a cor da letra do console
+                Console.WriteLine("------------------");
+                Console.WriteLine();
+                Console.WriteLine("Bem-vind@");
+                Console.WriteLine();
+                Console.WriteLine("------------------");
+                Console.WriteLine();
+
+                Console.WriteLine("Digite 3 números: ");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int n3 = int.Parse(Console.ReadLine());
+
+                if (n1 == n2 && n1 == n3)
+                {
+                    Console.WriteLine("Os três números são iguais!");
+                }
+                else if (n1 > n2 && n1 > n3)
+                {
+                    Console.WriteLine("O maior número é " + n1);
+                }
+                else if (n2 > n1 && n2 > n3)
+                {
+                    Console.WriteLine("O maior número é " + n2);
+                }
+                else
+                {
+                    Console.WriteLine("O maior número é " + n3);
+                }
+
+                Continuar();
+            }
+
+            static void Continuar() // pergunta se o usuário deseja continuar
+            {
+                Console.WriteLine();
+                Console.Write("Deseja continuar (s/n)? ");
+                string continuar = Console.ReadLine();
+
+                if (continuar == "s" || continuar == "S")
+                {
+                    MaiorNumero();
+                }
+                else
+                {
+                    Console.WriteLine("Obrigad@!");
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
+
+
--- Teste_02/Program.cs
+++ Teste_02/Program.cs
@@ -6,7 +6,13 @@ namespace Teste_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Digite o preço da carga: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o peso da carga: ");
+            double peso = double.Parse(Console.ReadLine());
+
+
         }
     }
 }
